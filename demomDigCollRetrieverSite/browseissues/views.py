d8b633107421e9c14bea69cef2f200a935936edf
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-07-20 16:33:17 -0500

    more work on basic functionality

--- demomDigCollRetrieverSite/browseissues/views.py
+++ demomDigCollRetrieverSite/browseissues/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
-from anIssue.models import Publication, AnIssuePage
+from anIssue.models import Publication, AnIssuePage, PublicationType
 
 # Create your views here.
 
 def browse_issues_by_publication_type(request):
     publication_type = request.GET.get('publication_type')
-    results = Publication.objects.filter(the_type__publication_type=publication_type)
+    results = Publication.objects.filter(the_type__publication_type=publication_type[0].upper()+publication_type[1:])
+    print(Publication.objects.all())
+    print(results)
     return render(request,
                   "browseissues/browse.html",
                   {'results': results}
@@ -25,4 +27,11 @@ def browse_for_dates(request):
     return render(request,
                   "browseissues/browse_dates.html",
                   {'results': date_dict}
+                 )
+
+def browse_issues_for_a_publication(request):
+    results = AnIssuePage.objects.filter(issue_page__publication_title=request.GET.get('publication_title'))
+    return render(request,
+                  "browseissues/browse_issues_by_title.html",
+                  {'results': results}
                  )
